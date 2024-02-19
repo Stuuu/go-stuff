c1d4e02b9030760c0509ec63ac3590c4831c21e5
Stuuu <stuartschrick@gmail.com>
2024-02-19 18:15:03 -0500

    04 Strings part pointers

--- cmd/main.go
+++ cmd/main.go
@@ -10,4 +10,19 @@ func main() {
 	b := []byte(s)
 	fmt.Printf("%8T %[1]v %d\n", b, len(b))
 
+	s2 := "the quick brown fox"
+
+	a := len(s2)
+	fmt.Println(a)
+	b2 := s2[:3]
+	fmt.Println(b2)
+	c := s2[4:9]
+	fmt.Println(c)
+	d := s2[:4] + "slow" + s2[9:]
+	fmt.Println(d)
+
+	s2 += "es"
+
+	fmt.Println(s2)
+
 }
