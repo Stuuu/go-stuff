8e5c38a2f7e92e395d9607afc3f413e928c87e15
Stuuu <stuartschrick@gmail.com>
2024-02-19 17:05:48 -0500

    03 basic types

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"hello"
-)
+import "fmt"
 
 func main() {
-	fmt.Println(hello.Say(os.Args[1:]))
+	a := 2
+	b := 3.1
+
+	fmt.Printf("a: %8T %[1]v\n", a)
+	fmt.Printf("b: %8T %[1]v\n", b)
+
+	a = int(b)
+
+	fmt.Printf("a: %8T %[1]v\n", a)
+
+	b = float64(a)
+	fmt.Printf("b: %8T %[1]v\n", b)
 }
