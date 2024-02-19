f2ad2c08979debb588a0cbbeb7253cef85ad782a
Stuuu <stuartschrick@gmail.com>
2024-02-19 17:27:25 -0500

    03 basic types consts, read from stdin

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,30 @@
 package main
 
-func main() {
-	const (
-		a       = 1
-		b       = 2 * 1024
-		c       = b << 3
-		g uint8 = 0x07
-		h uint8 = g & 0x03
-	)
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	var sum float64
+	var n int
+
+	for {
+		var val float64
+
+		_, err := fmt.Fscanln(os.Stdin, &val)
+		if err != nil {
+			break
+		}
+
+		sum += val
+		n++
+	}
+
+	if n == 0 {
+		fmt.Fprintln(os.Stderr, "no values")
+		os.Exit(-1)
+	}
+
+	fmt.Println("The average is", sum/float64(n))
 }
