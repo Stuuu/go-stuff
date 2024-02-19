528007a27c53c99ff7d3cf7ce9652636c608c040
Stuuu <stuartschrick@gmail.com>
2024-02-19 18:01:26 -0500

    04 Strings part 1 byte, rune unicode

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
+import "fmt"
 
 func main() {
-	var sum float64
-	var n int
+	s := "élite"
 
-	for {
-		var val float64
+	fmt.Printf("%8T %[1]v %d\n", s, len(s))
+	fmt.Printf("%8T %[1]v\n", []rune(s))
+	b := []byte(s)
+	fmt.Printf("%8T %[1]v %d\n", b, len(b))
 
-		_, err := fmt.Fscanln(os.Stdin, &val)
-		if err != nil {
-			break
-		}
-
-		sum += val
-		n++
-	}
-
-	if n == 0 {
-		fmt.Fprintln(os.Stderr, "no values")
-		os.Exit(-1)
-	}
-
-	fmt.Println("The average is", sum/float64(n))
 }
