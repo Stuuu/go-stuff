3c20c58a4269e5a01223ab61b3383a7e59c19338
Stuuu <stuartschrick@gmail.com>
2024-02-19 19:12:08 -0500

    Go Class: 05 Arrays, Slices, and Maps | map basics

--- cmd/main.go
+++ cmd/main.go
@@ -2,25 +2,17 @@ package main
 
 import "fmt"
 
-var w = [...]int{1, 2, 3}
-var x = []int{0, 0, 0}
-
 func main() {
-	y := do(w, x)
-	fmt.Println(w, x, y)
+	var m map[string]int
+	p := make(map[string]int)
 
-	z := w[:1]
-	fmt.Println(z)
-}
+	a := p["the"]
+	b := m["the"]
+	fmt.Println(a, b)
+	p["and"] = 1 // PANIC - nil map
+	m = p
+	m["and"]++
+	c := p["and"]
 
-func do(a [3]int, b []int) []int {
-	// a = b    // SYNTAX ERROR
-	a[0] = 4 // w unchanged
-	b[0] = 3 // x changed
-
-	c := make([]int, 5) // []int{0,0,0,0,0}
-	c[4] = 42
-	copy(c, b) // copies only 3 elts
-
-	return c
+	fmt.Println(m, p, c)
 }
