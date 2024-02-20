836c3b68505b711cc4e67268272ae5334781117c
Stuuu <stuartschrick@gmail.com>
2024-02-19 19:02:15 -0500

    Go Class: 05 Arrays, Slices, and Maps | more slices and  arrays

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,25 @@ package main
 
 import "fmt"
 
+var w = [...]int{1, 2, 3}
+var x = []int{0, 0, 0}
+
 func main() {
+	y := do(w, x)
+	fmt.Println(w, x, y)
 
-	t := []byte("string")
+	z := w[:1]
+	fmt.Println(z)
+}
 
-	fmt.Println(len(t), t)
-	fmt.Println(t[2])
-	fmt.Println(t[:2])
-	fmt.Println(t[2:])
-	fmt.Println(t[3:5], len(t[3:5]))
+func do(a [3]int, b []int) []int {
+	// a = b    // SYNTAX ERROR
+	a[0] = 4 // w unchanged
+	b[0] = 3 // x changed
+
+	c := make([]int, 5) // []int{0,0,0,0,0}
+	c[4] = 42
+	copy(c, b) // copies only 3 elts
+
+	return c
 }
