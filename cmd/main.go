4f5b603ac49d2c5cb3a707b94503e5bb37de652d
Stuuu <stuartschrick@gmail.com>
2024-02-19 20:44:10 -0500

    Go Class: 06 Control Statements; declarations & types | declaration scope

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-)
+import "fmt"
 
-func getNum() int {
-	return rand.Intn(10-0) + 0
-}
 func main() {
-	switch a := getNum(); a {
-	case 0, 1, 2:
-		fmt.Println("case 1", a)
-	case 3, 4, 5, 6, 7, 8:
-		fmt.Println("case 2", a)
-	default:
-		fmt.Println("case 3", a)
-	}
 
-	a := getNum()
-	// Switch on true
-	switch {
-	case a <= 2:
-		fmt.Println("less than 2", a)
-	case a <= 8:
-		fmt.Println("greater than 2 less than 8", a)
-	}
+	n, err := fmt.Println("Hello, playground")
+	fmt.Println(err)
 
+	if _, err := fmt.Println(n); err != nil {
+		fmt.Println(err)
+	}
 }
