61f00c8a22ec9f2c6a47164983fc9565fbbfdcab
Stuuu <stuartschrick@gmail.com>
2024-02-19 20:25:40 -0500

    Go Class: 06 Control Statements; declarations & types | switch statment

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
+func getNum() int {
+	return rand.Intn(10-0) + 0
+}
 func main() {
-	a := "a"
-	b := "a"
-	if a == b {
-		fmt.Println("equals")
-	} else {
-		fmt.Println("does not equal")
+	switch a := getNum(); a {
+	case 0, 1, 2:
+		fmt.Println("case 1", a)
+	case 3, 4, 5, 6, 7, 8:
+		fmt.Println("case 2", a)
+	default:
+		fmt.Println("case 3", a)
 	}
 
-	for i := 0; i < 10; i++ {
-		fmt.Printf("(%d, %d)\n", i, i*i)
-	}
-
-outer:
-	for {
-		for i := 0; i < 100; i++ {
-			if i == 23 {
-				break outer
-			}
-			fmt.Println(i)
-		}
+	a := getNum()
+	// Switch on true
+	switch {
+	case a <= 2:
+		fmt.Println("less than 2", a)
+	case a <= 8:
+		fmt.Println("greater than 2 less than 8", a)
 	}
 
 }
