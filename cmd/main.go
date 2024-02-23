93b5d2e60edc02bfb5b0267f1c9d0304b79b23fa
Stuuu <stuartschrick@gmail.com>
2024-02-22 21:52:48 -0500

    Go Class: 07 Formatted & File I/O | file i/o basic cat example

--- cmd/main.go
+++ cmd/main.go
@@ -1,35 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	s := []int{1, 2, 3}
+	for _, fname := range os.Args[1:] {
+		file, err := os.Open(fname)
 
-	fmt.Printf("%T\n", s)
-	fmt.Printf("%v\n", s)
-	fmt.Printf("%#v\n", s)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
 
-	fmt.Println()
+		if _, err := io.Copy(os.Stdout, file); err != nil {
+			fmt.Fprint(os.Stderr, err)
+			continue
+		}
 
-	a := [3]rune{'a', 'b', 'c'}
-	fmt.Printf("%T\n", a)
-	fmt.Printf("%v\n", a)
-	fmt.Printf("%#v\n", a)
-	fmt.Printf("%q\n", a)
+		file.Close()
 
-	fmt.Println()
-	m := map[string]int{"and": 1, "or": 2}
-	fmt.Printf("%T\n", m)
-	fmt.Printf("%v\n", m)
-	fmt.Printf("%#v\n", m)
-
-	fmt.Println()
-
-	f := "a string"
-	b := []byte(f)
-
-	fmt.Printf("%T\n", b)
-	fmt.Printf("%v\n", b)
-	fmt.Printf("%#v\n", b)
-	fmt.Printf("%q\n", b)
+	}
 }
