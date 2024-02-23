81965d4e9e60f09cc4f7b358be901793a150be62
Stuuu <stuartschrick@gmail.com>
2024-02-22 21:07:23 -0500

    Go Class: 07 Formatted & File I/O | fmt col spacing

--- cmd/main.go
+++ cmd/main.go
@@ -6,9 +6,15 @@ func main() {
 	a, b := 12, 345
 	c, d := 1.2, 3.45
 
-	fmt.Printf("%d %d\n", a, b)
-	fmt.Printf("%X %X\n", a, b)
-	fmt.Printf("%#x %#x\n", a, b)
-	fmt.Printf("%f %.5f\n", c, d)
+	fmt.Println()
 
+	fmt.Printf("|%9d| |%9d|", a, b)
+	fmt.Println()
+	fmt.Printf("|%09d| |%09d|", a, b)
+	fmt.Println()
+	fmt.Printf("|%-9d| |%-9d|", a, b)
+
+	fmt.Println()
+
+	fmt.Printf("|%9f| |%9.2f|", c, d)
 }
