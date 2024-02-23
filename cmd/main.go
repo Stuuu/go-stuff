00d3afdeb31e6270a4e935f9f89d8f0756815674
Stuuu <stuartschrick@gmail.com>
2024-02-22 22:21:08 -0500

    Go Class: 07 Formatted & File I/O | file i/o making a basic version of wc

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, fname := range os.Args[1:] {
+		var lc, wc, cc int
 		file, err := os.Open(fname)
 
 		if err != nil {
@@ -15,14 +17,17 @@ func main() {
 			continue
 		}
 
-		data, err := io.ReadAll(file)
-		if err != nil {
-			fmt.Fprint(os.Stderr, err)
-			continue
+		scan := bufio.NewScanner(file)
+
+		for scan.Scan() {
+			s := scan.Text()
+
+			wc += len(strings.Fields(s))
+			cc += len(s)
+			lc++
 		}
 
-		fmt.Println("The file has", len(data), "bytes")
-
+		fmt.Printf(" %7d %7d %7d %s\n", lc, wc, cc, fname)
 		file.Close()
 
 	}
