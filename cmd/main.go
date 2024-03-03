00feceba0d0227cce5a2c60b2746e25bcbc8cfc8
Stuuu <stuartschrick@gmail.com>
2024-03-03 18:44:57 -0500

    Go Class: 12 Structs, Struct tags & JSOn | struct example of unexported private field

--- cmd/main.go
+++ cmd/main.go
@@ -7,20 +7,19 @@ import (
 
 type Response struct {
 	Page  int      `json:"page"`
-	Words []string `json:"words,omitempty"`
+	words []string `json:"words"`
 }
 
 func main() {
 
 	r := &Response{Page: 1}
 	j, _ := json.Marshal(r)
-	fmt.Printf("%#v\n", r)
 	fmt.Println(string(j))
+	fmt.Printf("%#v\n", r)
 
 	var r2 Response
 
-	_ = json.Unmarshal(j, &r2)
-
 	fmt.Printf("%#v\n", r2)
+	_ = json.Unmarshal(j, &r2)
 
 }
