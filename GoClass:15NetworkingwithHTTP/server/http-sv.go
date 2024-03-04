12c04f061da599a5e45b21f8d8b494921fac75cc
Stuuu <stuartschrick@gmail.com>
2024-03-03 20:42:52 -0500

    GoClass:15NetworkingwithHTTP | adjusting the server to become a client of json typicode

--- GoClass:15NetworkingwithHTTP/server/http-sv.go
+++ GoClass:15NetworkingwithHTTP/server/http-sv.go
@@ -1,13 +1,57 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
+	"text/template"
 )
 
+type todo struct {
+	UserID    int    `json:"UserID"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+var form = `
+<h1>Todo #{{.ID}}</h1>
+<div>{{printf "User %d" .UserID}}</div>
+<div>{{printf "%s (completed: %t)" .Title .Completed}}</div>
+`
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, world! from %s\n", r.URL.Path[1:])
+	const base = "https://jsonplaceholder.typicode.com/"
+	resp, err := http.Get(base + r.URL.Path[1:])
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+
+	var item todo
+
+	err = json.Unmarshal(body, &item)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+
+	tmpl := template.New("mine")
+
+	tmpl.Parse(form)
+	tmpl.Execute(w, item)
 }
 
 func main() {
