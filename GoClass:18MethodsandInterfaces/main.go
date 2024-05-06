57349f52fe0511bc2b66f863880e296ef5381807
stu <stuartschrick@gmail.com>
2024-05-05 20:31:29 -0400

    GoClass:18MethodsandInterfaces | methods and interfaces

--- GoClass:18MethodsandInterfaces/main.go
+++ GoClass:18MethodsandInterfaces/main.go
@@ -19,28 +19,18 @@ func (l Line) Distance() float64 {
 	return math.Hypot(l.End.X - l.Begin.X, l.End.Y - l.Begin.Y)
 }
 
-func (p Path) Distance() (sum float64) {
-	for i:= 1; i < len(p); i++ {
-		sum += Line{p[i-1], p[i]}.Distance()
-	}
-	
-	return sum
-}
 
-type Distancer interface {
-	Distance() float64
+func (l Line) ScaleBy(f float64) Line {
+	l.End.X += (f-1)*(l.End.X - l.Begin.X)
+	l.End.Y += (f-1)*(l.End.Y - l.Begin.Y)
+	return Line{l.Begin, Point{l.End.X, l.End.Y}}
 }
-
-func PrintDistance(d Distancer) {
-	fmt.Println(d.Distance())
-}
-
 
 func main() {
-	side := Line{Point{1,3}, Point{4,6}}
-	perimeter := Path{{1,1}, {5,1},{5,4}, {1,1}}
-	
-	PrintDistance(side)
-	PrintDistance(perimeter)
+	side := Line{Point{1,2}, Point{4,6}}
+
+	s2 := side.ScaleBy(2.5)
+	fmt.Println(s2.Distance())
+	fmt.Println(Line{Point{1,2}, Point{4,6}}.ScaleBy(2).Distance())	
 
 }
