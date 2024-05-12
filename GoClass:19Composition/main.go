70496c4bd186a536cad674497f35ad4150ccacfd
stu <stuartschrick@gmail.com>
2024-05-12 18:45:00 -0400

    GoClass:19Composition | making nil useful part 1

--- GoClass:19Composition/main.go
+++ GoClass:19Composition/main.go
@@ -2,35 +2,31 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
-type Organ struct {
-	Name   string
-	Weight int
+type StringStack struct {
+	data []string
 }
 
-type Organs []Organ
-
-func (s Organs) Len() int      { return len(s) }
-func (s Organs) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
-
-type ByName struct{ Organs }
-type ByWeight struct{ Organs }
-
-func (s ByName) Less(i, j int) bool {
-	return s.Organs[i].Name < s.Organs[j].Name
-}
-func (s ByWeight) Less(i, j int) bool {
-	return s.Organs[i].Weight < s.Organs[j].Weight
+func (s *StringStack) Push(x string) {
+	s.data = append(s.data, x)
 }
 
-func main() {
-	s := []Organ{{"brain", 1340}, {"liver", 1494}, {"spleen", 162}, {"pancreas", 131}, {"heart", 290}}
+func (s *StringStack) Pop() string {
+	if l := len(s.data); l > 0 {
+		t := s.data[l-1]
+		s.data = s.data[:l-1]
+		return t
+	}
 	
-	sort.Sort(ByWeight{s})
-	fmt.Println(s)
-	sort.Sort(ByName{s})
-	sort.Sort(sort.Reverse(ByName{s}))
-	fmt.Println(s)
+	panic("pop from empty stack")
+}
+func main() {
+
+	stack := StringStack{}
+	
+	stack.Push("test1")
+	stack.Push("test2")
+	fmt.Println(stack.Pop())
+	fmt.Println(stack.Pop())
 }
