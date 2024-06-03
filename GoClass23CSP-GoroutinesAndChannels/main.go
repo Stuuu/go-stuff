9ef604b91115f1169020da624c7b548184465f60
stu <stuartschrick@gmail.com>
2024-06-02 21:44:45 -0400

    GoClass23CSP-GoroutinesAndChannels | channels example 2

--- GoClass23CSP-GoroutinesAndChannels/main.go
+++ GoClass23CSP-GoroutinesAndChannels/main.go
@@ -1,52 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	"time"
 )
 
-type result struct {
-	url     string
-	err     error
-	latency time.Duration
+type nextCh chan int
+
+func (ch nextCh) handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1> You got %d<h1>", <-ch)
 }
 
-func get(url string, ch chan<- result) {
-	start := time.Now()
-
-	if resp, err := http.Get(url); err != nil {
-		ch <- result{url, err, 0}
-	} else {
-		t := time.Since(start).Round(time.Millisecond)
-		ch <- result{url, nil, t}
-		resp.Body.Close()
+func counter(ch chan<- int) {
+	for i := 0; ; i++ {
+		ch <- i
 	}
 }
-
 func main() {
-	results := make(chan result)
-	list := []string{
-		"https://amazon.com",
-		"https://google.com",
-		"https://x.com",
-		"https://bing.com",
-		"https://wsj.com",
-		"https://nytimes.com",
-	}
-	
-	for _, url := range list {
-		go get(url, results)
-	}
-	
-	for range list {
-		r := <- results
-		
-		if r.err != nil {
-			log.Printf("%-20s %s\n", r.url, r.err)
-		} else {
-			log.Printf("%-20s %s\n", r.url, r.latency)
-		}
-	}
-
+	var nextID nextCh = make(chan int)
+	go counter(nextID)
+	http.HandleFunc("/", nextID.handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
