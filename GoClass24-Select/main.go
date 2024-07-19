32a9ba5edb2f7e374bbbfbb9e388b653e068168f
stu <stuartschrick@gmail.com>
2024-07-18 21:12:48 -0400

    GoClass24-Select example select with ticker to set channel read inc limit

--- GoClass24-Select/main.go
+++ GoClass24-Select/main.go
@@ -2,58 +2,26 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"time"
 )
 
-type result struct {
-	url     string
-	err     error
-	latency time.Duration
-}
-
-func get(url string, ch chan<- result) {
-	start := time.Now()
-
-	if resp, err := http.Get(url); err != nil {
-		ch <- result{url, err, 0}
-	} else {
-		t := time.Since(start).Round(time.Millisecond)
-		ch <- result{url, nil, t}
-		resp.Body.Close()
-	}
-}
-
 func main() {
-	
-	stopper := time.After(300 *time.Millisecond)
-	results := make(chan result)
-	list := []string{
-		"https://amazon.com",
-		"https://google.com",
-		"https://x.com",
-		"https://bing.com",
-		"https://wsj.com",
-		"https://nytimes.com",
-	}
-	
-	for _, url := range list {
-		go get(url, results)
-	}
-	
-	for range list {
-		select {
-		case r := <- results:
-			if r.err != nil {
-				log.Printf("%-20s %s\n", r.url, r.err)
-			} else {
-				log.Printf("%-20s %s\n", r.url, r.latency)
-			}
-			
-		case <-stopper:
-			log.Fatal("timeout")
-		}
-		
-	}
+	log.Println("start")
+	const tickRate = 2 * time.Second
 
+	stopper := time.After(5 * tickRate)
+	ticker := time.NewTicker(tickRate).C
+
+	loop:
+		for {
+			select {
+			case <-ticker:
+				log.Println("tick")
+				
+			case <-stopper:
+				break loop
+			}
+		}
+
+	log.Println("finish")
 }
