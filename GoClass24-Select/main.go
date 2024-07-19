41c0356226f3e5e8ba11c46ba0fa872528850ede
stu <stuartschrick@gmail.com>
2024-07-18 21:05:34 -0400

    GoClass24-Select example select with a time after channel used to timeout request

--- GoClass24-Select/main.go
+++ GoClass24-Select/main.go
@@ -2,32 +2,58 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"time"
 )
 
+type result struct {
+	url     string
+	err     error
+	latency time.Duration
+}
+
+func get(url string, ch chan<- result) {
+	start := time.Now()
+
+	if resp, err := http.Get(url); err != nil {
+		ch <- result{url, err, 0}
+	} else {
+		t := time.Since(start).Round(time.Millisecond)
+		ch <- result{url, nil, t}
+		resp.Body.Close()
+	}
+}
 
 func main() {
-	chans := []chan int{
-		make(chan int),
-		make(chan int),
+	
+	stopper := time.After(300 *time.Millisecond)
+	results := make(chan result)
+	list := []string{
+		"https://amazon.com",
+		"https://google.com",
+		"https://x.com",
+		"https://bing.com",
+		"https://wsj.com",
+		"https://nytimes.com",
 	}
 	
-	for i := range chans {
-		go func(i int, ch chan<- int) {
-			
-			for {
-				time.Sleep(time.Duration(i) * time.Second)
-				ch <- i
-			}
-		}(i+1, chans[i])
+	for _, url := range list {
+		go get(url, results)
 	}
 	
-	for i := 0; i < 12; i ++ {
+	for range list {
 		select {
-		case m0 := <-chans[0]:
-			log.Println("recieved", m0)
-		case m1 := <-chans[1]:
-			log.Println("recieved", m1)
+		case r := <- results:
+			if r.err != nil {
+				log.Printf("%-20s %s\n", r.url, r.err)
+			} else {
+				log.Printf("%-20s %s\n", r.url, r.latency)
+			}
+			
+		case <-stopper:
+			log.Fatal("timeout")
 		}
+		
 	}
+
 }
