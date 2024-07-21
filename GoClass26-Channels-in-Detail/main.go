0f72dd4afbbd2548986a03c28263b8fef661a9e3
stu <stuartschrick@gmail.com>
2024-07-20 21:43:15 -0400

    GoClass26 example of how buffered channels can enable race conditions

--- GoClass26-Channels-in-Detail/main.go
+++ GoClass26-Channels-in-Detail/main.go
@@ -2,14 +2,40 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+
+type T struct {
+	i byte
+	b bool
+}
+
+func send(i int, ch chan<- *T){
+	t := &T{i: byte(i)}
+	ch <- t
+	
+	t.b = true // UNSAFE AT ANY SPEED
+}
+
 func main() {
-	messages := make(chan string, 2)
+	vs := make([]T, 5)
+	ch := make(chan *T, 5)
 	
-	messages <- "buffered"
-	messages <- "channel"
+	for i := range vs {
+		go send(i, ch)
+	}
 	
-	fmt.Println(<-messages)
-	fmt.Println(<-messages)
+	time.Sleep(1 * time.Second) // all goroutines started 
+	
+	// copy quickly
+	for i := range vs {
+		vs[i] = *<-ch 
+	}
+	
+	// print later 
+	for _, v := range vs {
+		fmt.Println(v)
+	}
+	
 }
